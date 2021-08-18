52904855a9d4c0702a4c12a51e4a69efcf8e3db7
azdek <azdek.abdykadyr@gmail.com>
2021-08-18 17:12:54 +0600

    End

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import clx from 'classnames'
+
+const TOTAL = 2816105
+const FIRST_PAYMENT = 503865
+const MONTHLY_PAYMENT = 128500
+
+const GRAPH = [FIRST_PAYMENT]
+const monthNames = ["Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", "Январь", "Февраль", "Март", "April", "Май", "Июнь",
+  "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь"
+];
+
+for (let i = 0; i < 17; i++) {
+  GRAPH.push(MONTHLY_PAYMENT)
+}
+GRAPH.push(TOTAL - GRAPH.reduce((a, b) => a + b, 0))
+
 
 function App() {
+  const PAYED = Number(window.prompt('Введите сумму'))
+  const table = []
+  let totalGraph = 0
+
+  GRAPH.forEach((sum, i) => {
+    totalGraph += sum
+    table.push(
+      <tr key={i} className={clx({ payed: totalGraph <= PAYED })}>
+        <td>{monthNames[i]}</td>
+        <td>{sum}</td>
+        <td>{PAYED - totalGraph}</td>
+      </tr>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Месяц</th>
+            <th>Нужно</th>
+            <th>Остаток</th>
+          </tr>
+        </thead>
+        <tbody>
+          {table}
+        </tbody>
+      </table>
+    </>
   );
 }
 
